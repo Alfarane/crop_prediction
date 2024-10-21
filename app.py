f7a321578b897dcf2dc30a2022d6626f9987563a
Alfarane <alpharane22@gmail.com>
2024-10-21 12:40:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask,jsonify,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    float_feature=[float(x) for x in request.form.values()]
+    features = [np.array(float_feature)]
+    prediction = model.predict(features)
+    return render_template("index.html",prediction_text="The Predicted Crop is {}".format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
